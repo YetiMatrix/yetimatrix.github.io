f3b357043749d6b05a76eed6937aa1748065d823
blackarrowmatrix <81826250+blackarrowmatrix@users.noreply.github.com>
2021-04-08 21:33:31 +0100

    ok

--- addons_xml_generator3.py
+++ addons_xml_generator3.py
@@ -1,144 +1,112 @@
 # *
-# *  Copyright (C) 2012-2013 Garrett Brown
-# *  Copyright (C) 2010      j48antialias
+# * Copyright (C) 2012-2013 Garrett Brown
+# * Copyright (C) 2010 j48antialias
 # *
-# *  This Program is free software; you can redistribute it and/or modify
-# *  it under the terms of the GNU General Public License as published by
-# *  the Free Software Foundation; either version 2, or (at your option)
-# *  any later version.
+# * Este Programa é um software livre; você pode redistribuí-lo e / ou modificar
+# * sob os termos da GNU General Public License conforme publicada por
+# * a Free Software Foundation; tanto a versão 2, ou (por sua opção)
+# * qualquer versão posterior.
 # *
-# *  This Program is distributed in the hope that it will be useful,
-# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# *  GNU General Public License for more details.
+# * Este programa é distribuído na esperança de que seja útil,
+# * mas SEM QUALQUER GARANTIA; sem mesmo a garantia implícita de
+# * COMERCIALIZAÇÃO ou ADEQUAÇÃO A UM DETERMINADO FIM. Veja o
+# * GNU General Public License para obter mais detalhes.
 # *
-# *  You should have received a copy of the GNU General Public License
-# *  along with XBMC; see the file COPYING.  If not, write to
-# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-# *  http://www.gnu.org/copyleft/gpl.html
+# * Você deve ter recebido uma cópia da GNU General Public License
+# * junto com o XBMC; veja o arquivo COPYING. Se não, escreva para
+# * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# * http://www.gnu.org/copyleft/gpl.html
 # *
-# *  Based on code by j48antialias:
-# *  https://anarchintosh-projects.googlecode.com/files/addons_xml_generator.py
+# * Baseado no código de j48antialias:
+# * https://anarchintosh-projects.googlecode.com/files/addons_xml_generator.py
  
-""" addons.xml generator """
+"" "gerador addons.xml" ""
  
-import os
-import sys
-import hashlib
+importar  os
+import  sys
  
-# Compatibility with 3.0, 3.1 and 3.2 not supporting u"" literals
-if sys.version < '3':
-    import codecs
-    def u(x):
-        return codecs.unicode_escape_decode(x)[0]
-else:
-    def u(x):
-        return x
+# Compatibilidade com 3.0, 3.1 e 3.2 não suportando literais u ""
+se  sys . versão  <  '3' :
+    importar  codecs
+    def  u ( x ):
+        retornar  codecs . unicode_escape_decode ( x ) [ 0 ]
+mais :
+    def  u ( x ):
+        retornar  x
  
-class Generator:
-    """
-        Generates a new addons.xml file from each addons addon.xml file
-        and a new addons.xml.md5 hash file. Must be run from the root of
-        the checked-out repo. Only handles single depth folder structure.
-    """
-    def __init__( self ):
-        # generate files
-        self._generate_addons_file()
-        self._generate_md5_file()
-        # notify user
-        print("Finished updating addons xml and md5 files")
+classe  Generator :
+    "" "
+        Gera um novo arquivo addons.xml a partir de cada arquivo addon.xml addons
+        e um novo arquivo hash addons.xml.md5. Deve ser executado a partir da raiz de
+        o repositório com check-out. Só lida com estrutura de pasta de profundidade única.
+    "" "
+    def  __init__ ( self ):
+        # gerar arquivos
+        eu . _generate_addons_file ()
+        eu . _generate_md5_file ()
+        # notificar usuário
+        print ( "Atualização de arquivos xml e md5 de complementos concluída" )
  
-    def _generate_addons_file( self ):
-        # addon list
-        addons = os.listdir( "." )
-        # final addons text
-        addons_xml = u("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<addons>\n")
-        # loop thru and add each addons addon.xml file
-        for addon in addons:
-            try:
-                # skip any file or .svn folder or .git folder
-                if ( not os.path.isdir( addon ) or addon == ".svn" or addon == ".git" ): continue
-                # create path
-                _path = os.path.join( addon, "addon.xml" )
-                # split lines for stripping
-                xml_lines = open( _path, "r" , encoding="UTF-8").read().splitlines()
-                # new addon
-                addon_xml = ""
-                # loop thru cleaning each line
-                for line in xml_lines:
-                    # skip encoding format line
-                    if ( line.find( "<?xml" ) >= 0 ): continue
-                    # add line
-                    if sys.version < '3':
-                        addon_xml += unicode( line.rstrip() + "\n", "UTF-8" )
-                    else:
-                        addon_xml += line.rstrip() + "\n"
-                # we succeeded so add to our final addons.xml text
-                addons_xml += addon_xml.rstrip() + "\n\n"
-            except Exception as e:
-                # missing or poorly formatted addon.xml
-                print("Excluding %s for %s" % ( _path, e ))
-        # clean and add closing tag
-        addons_xml = addons_xml.strip() + u("\n</addons>\n")
-        # save file
-        self._save_file( addons_xml.encode( "UTF-8" ), file="addons.xml" )
+    def  _generate_addons_file ( self ):
+        # lista de complementos
+        addons  =  os . listdir ( "." )
+        # texto final de addons
+        addons_xml  =  u ( "<? xml version = \" 1.0 \ " encoding = \" UTF-8 \ " standalone = \" yes \ " ?> \ n <addons> \ n " )
+        # loop thru e adicionar cada arquivo addon.xml addons
+        para  addon  em  addons :
+            tente :
+                # ignorar qualquer arquivo ou pasta .svn ou pasta .git
+                if ( not  os . path . isdir ( addon ) ou  addon  ==  ".svn"  ou  addon  ==  ".git" ): continue
+                # criar caminho
+                _path  =  os . caminho . juntar ( addon , "addon.xml" )
+                # linhas de divisão para decapagem
+                xml_lines  =  open ( _path , "r" ). leia (). linhas de divisão ()
+                # novo addon
+                addon_xml  =  ""
+                # loop através da limpeza de cada linha
+                para  linha  em  xml_lines :
+                    # pular linha de formato de codificação
+                    if ( line . find ( "<? xml" ) > =  0 ): continue
+                    # adicionar linha
+                    se  sys . versão  <  '3' :
+                        addon_xml  + =  unicode ( line . rstrip () +  " \ n " , "UTF-8" )
+                    mais :
+                        addon_xml  + =  linha . rstrip () +  " \ n "
+                # conseguimos, então adicione ao nosso texto addons.xml final
+                addons_xml  + =  addon_xml . rstrip () +  " \ n \ n "
+            exceto  exceção  como  e :
+                # addon.xml ausente ou mal formatado
+                print ( "Excluindo% s para% s"  % ( _path , e ))
+        # limpar e adicionar tag de fechamento
+        addons_xml  =  addons_xml . strip () +  u ( " \ n </addons> \ n " )
+        # salvar Arquivo
+        eu . _save_file ( addons_xml . encode ( "UTF-8" ), arquivo = "addons.xml" )
  
-    def _generate_md5_file( self ):
-        # create a new md5 hash
-        try:
-            import md5
-            m = md5.new( open( "addons.xml", "r" ).read() ).hexdigest()
-        except ImportError:
-            import hashlib
-            m = hashlib.md5( open( "addons.xml", "r", encoding="UTF-8" ).read().encode( "UTF-8" ) ).hexdigest()
+    def  _generate_md5_file ( self ):
+        # criar um novo hash md5
+        tente :
+            importar  md5
+            m  =  md5 . novo ( open ( "addons.xml" , "r" ). read ()). hexdigest ()
+        exceto  ImportError :
+            importar  hashlib
+            m  =  hashlib . md5 ( open ( "addons.xml" , "r" , encoding = "UTF-8" ). read (). encode ( "UTF-8" )). hexdigest ()
  
-        # save file
-        try:
-            self._save_file( m.encode( "UTF-8" ), file="addons.xml.md5" )
-        except Exception as e:
-            # oops
-            print("An error occurred creating addons.xml.md5 file!\n%s" % e)
+        # salvar Arquivo
+        tente :
+            eu . _save_file ( m . encode ( "UTF-8" ), arquivo = "addons.xml.md5" )
+        exceto  exceção  como  e :
+            # ops
+            print ( "Ocorreu um erro ao criar o arquivo addons.xml.md5! \ n % s"  %  e )
  
-    def _save_file( self, data, file ):
-        try:
-            # write data to the file (use b for Python 3)
-            open( file, "wb" ).write( data )
-        except Exception as e:
-            # oops
-            print("An error occurred saving %s file!\n%s" % ( file, e ))
+    def  _save_file ( self , data , file ):
+        tente :
+            # grava dados no arquivo (use b para Python 3)
+            abrir ( arquivo , "wb" ). escrever ( dados )
+        exceto  exceção  como  e :
+            # ops
+            print ( "Ocorreu um erro ao salvar% s arquivo! \ n % s"  % ( arquivo , e ))
  
  
-if ( __name__ == "__main__" ):
-    # start
-    Generator()
-	
-parent_folder = './'
-all_dirs = []
-for dir_name in os.listdir(parent_folder):
-    subdir = os.path.join(parent_folder, dir_name)
-    if os.path.isdir(subdir) and dir_name not in [".git", "externals"]:
-        # addon directory
-        all_dirs.append(dir_name)
-        html = "<html>\n<body>\n<h1>Directory listing for %s</h1>\n<hr/>\n<pre>" % dir_name
-        html += "<a href=\"../index.html\">..</a>\n"
-        for filename in os.listdir(subdir):
-            if filename.endswith(".zip"):
-                file = os.path.join(subdir, filename)
-                # append zip to html listing
-                html += "<a href=\"%s\">%s</a>\n" % (filename, filename)
-        html += "</pre>\n</body>\n</html>"
-        html_file = os.path.join(subdir, "index.html")
-        print ("write html file %s" % html_file)
-        with open(html_file, 'w') as f:
-            f.write(html)
-
-# write main index.html
-html = "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-for dir_name in all_dirs:
-    dir_path = os.path.join(parent_folder, dir_name, 'index.html')
-    html += "<a href=\"%s\">%s</a>\n" % (dir_path, dir_name)
-html += "</pre>\n</body>\n</html>"
-html_file = os.path.join(parent_folder, "index.html")
-print ("write html file %s" % html_file)
-with open(html_file, 'w') as f:
-    f.write(html)
+if ( __name__  ==  "__main__" ):
+    # começar
+    Gerador ()
